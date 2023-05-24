08590ff34dee2dc03d3fa998ced7d4cb6759241a
LucasNF22 <lucasnfiorentino@gmail.com>
2023-05-24 11:38:14 -0300

    Configuracion de idioma, estilos y  helpers

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -2,11 +2,11 @@
 export const Navbar = () => {
   return (
     <div className="navbar navbar-dark bg-dark mb-4 px-4">
-        <spam className="navbar-brand" >
+        <span className="navbar-brand" >
             <i className="fas fa-calendar-alt"></i>
             &nbsp;
             Lucas
-        </spam>
+        </span>
 
         <button className="btn btn-outline-danger" >
             <i className="fas fa-sign-out-alt" ></i>
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,58 @@
-import { Navbar } from "../components/Navbar"
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { addHours } from 'date-fns';
+
+
+import { Navbar } from "../components/Navbar";
+import { localizer, getMessagesES } from '../../helpers';
 
 export const CalendarPage = () => {
+
+
+  const events = [{
+    title: 'Cumpleanito',
+    notes: 'Tomar Fernet',
+    start: new Date(),
+    end: addHours( new Date(), 2 ),
+    bgColor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'Lucas'
+    }
+  }];
+
+  const eventStyleGetter = ( event, start, end, isSelected )=> {
+    console.log({ event, start, end, isSelected }); 
+    
+    const style = {
+      backgroundColor: '#347cf7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+    };
+
+    return{
+      style
+    };
+  };
+
+
   return (
     <>
       <Navbar />
+
+      <Calendar
+        culture= 'es'
+        messages= { getMessagesES() }
+        localizer={ localizer }
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+        eventPropGetter={ eventStyleGetter }
+      />
+
     </>
   )
 }
