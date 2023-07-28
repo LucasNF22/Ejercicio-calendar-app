68b72944301223ebad136d05f415b8b0668c17bc
LucasNF22 <lucasnfiorentino@gmail.com>
2023-07-28 15:42:18 -0300

    Configuracion de UseForm en login page, y conexion del login con el backend

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,18 +1,54 @@
 
+import { useAuthStore } from '../../hooks';
+import { useForm } from '../../hooks/useForm';
 import './LoginPage.css';
 
+
+const loginFormFields = {
+    loginEmail: '',
+    loginPassword: '',
+}
+
+const registerFormFields = {
+    registerName: '',
+    registerEmail: '',
+    registerPassword: '',
+    registerPassword2: '',
+}
+
 export const LoginPage = () => {
+
+    const { startLogin } = useAuthStore();
+
+    const { loginEmail, loginPassword, onInputChange:onLoginInputChange } = useForm( loginFormFields );
+    const { registerName, registerEmail, registerPassword, registerPassword2, onInputChange:onRegisterInputChange } = useForm( registerFormFields );
+
+    const loginSubmit = (e) => {
+        e.preventDefault();
+        startLogin({ email: loginEmail, password: loginPassword })
+
+    }
+
+    const registerSubmit = (e) => {
+        e.preventDefault();
+        console.log({ registerName, registerEmail, registerPassword, registerPassword2 });
+
+    }
+
     return (
         <div className="container login-container">
             <div className="row">
                 <div className="col-md-6 login-form-1">
                     <h3>Ingreso</h3>
-                    <form>
+                    <form onSubmit={ loginSubmit } >
                         <div className="form-group mb-2">
                             <input 
                                 type="text"
                                 className="form-control"
                                 placeholder="Correo"
+                                name= "loginEmail"
+                                value= { loginEmail }
+                                onChange= { onLoginInputChange }
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -20,6 +56,9 @@ export const LoginPage = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña"
+                                name= "loginPassword"
+                                value= { loginPassword }
+                                onChange= { onLoginInputChange }
                             />
                         </div>
                         <div className="d-grid gap-2">
@@ -34,12 +73,15 @@ export const LoginPage = () => {
 
                 <div className="col-md-6 login-form-2">
                     <h3>Registro</h3>
-                    <form>
+                    <form onSubmit={ registerSubmit } >
                         <div className="form-group mb-2">
                             <input
                                 type="text"
                                 className="form-control"
                                 placeholder="Nombre"
+                                name= "registerName"
+                                value= { registerName }
+                                onChange= { onRegisterInputChange }
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -47,6 +89,9 @@ export const LoginPage = () => {
                                 type="email"
                                 className="form-control"
                                 placeholder="Correo"
+                                name= "registerEmail"
+                                value= { registerEmail }
+                                onChange= { onRegisterInputChange }
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -54,6 +99,9 @@ export const LoginPage = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña" 
+                                name= "registerPassword"
+                                value= { registerPassword }
+                                onChange= { onRegisterInputChange }
                             />
                         </div>
 
@@ -62,6 +110,9 @@ export const LoginPage = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Repita la contraseña" 
+                                name= "registerPassword2"
+                                value= { registerPassword2 }
+                                onChange= { onRegisterInputChange }
                             />
                         </div>
 
--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,17 +1,35 @@
-import { useDispatch, useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import calendarApi from "../api/calendarApi";
 
 
-export const useAuthSlice = () => {
+export const useAuthStore = () => {
 
-    const { } = useSelector( state => state.auth );
+    const { status, user, errorMessage } = useSelector( state => state.auth );
     const dispatch = useDispatch();
 
+    const startLogin = async({ email, password }) => {
 
+        console.log({ email, password });
+
+        try {
+            const respuesta = await calendarApi.post('/auth', { email, password });
+            console.log(respuesta);
+
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }   
 
     return {
         // Propiedades
-
+        status, 
+        user, 
+        errorMessage,
 
         // Metodos
+        startLogin,
+
     }
 }
