ac9215c29cbe02ef3abae02b2fcbc45724993852
LucasNF22 <lucasnfiorentino@gmail.com>
2023-07-27 21:31:49 -0300

    Instalacion de axios, inicio de configuracion de endpoint del auth y hook del authStore. se incorporo el hoook useForm

---
+++ src/hooks/useAuthStore.js
@@ -0,0 +1,17 @@
+import { useDispatch, useDispatch, useSelector } from "react-redux"
+
+
+export const useAuthSlice = () => {
+
+    const { } = useSelector( state => state.auth );
+    const dispatch = useDispatch();
+
+
+
+    return {
+        // Propiedades
+
+
+        // Metodos
+    }
+}
